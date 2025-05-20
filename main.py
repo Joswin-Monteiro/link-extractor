8396c6ec6989d4f30317f4999d214f72d5ff1f5b
Joswin <joswinpresten20@gmail.com>
2025-05-20 19:57:48 +0530

    Changed some code and switched to lxml parser for speed

--- main.py
+++ main.py
@@ -10,31 +10,34 @@ python main.py [link]
 Example:
 python main.py https://www.google.com''')
 
+if __name__ == "__main__":
 # Extract link fromm command link argument
-link = ''
-try:
-    link = sys.argv[1]
-except IndexError:
-    help()
-    exit(1)
-
-if link == "-h" or link=="--help":
-    help()
-else:
-    # Request the code and extract links from the html page
+    link = ''
     try:
-        r = requests.get(link)
-    except:
-        print("Arr error occured!")
+        link = sys.argv[1]
+    except IndexError:
+        help()
         exit(1)
-    soup = BeautifulSoup(r.content.decode(), 'html.parser')
 
-    linkText = ""
+    if link == "-h" or link=="--help":
+        help()
+    else:
+        # Request the code and extract links from the html page
+        try:
+            r = requests.get(link)
+        except:
+            print("Arr error occured!")
+            exit(1)
+        soup = BeautifulSoup(r.content.decode(), 'lxml')
 
-    # Write the links to links.txt file
-    with open("links.txt", "a") as file:
+        linkText = ""
+
+        # Find all the links
         for i in soup.find_all('a'):
-            linkText += str(i) 
-        file.write(linkText)
+            linkText += str(i.get('href')) 
+        # Write links to links.txt file in current directory
+        with open("links.txt", "a") as file:
+            file.write(linkText)
 
-    print("Link extracted successfully to links.txt file in the current directory")
+        print("Link extracted successfully to links.txt file in the current directory")
+
